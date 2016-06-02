823504553e0e6d8cc0648cca23ee627562cd81ac
David Baldwynn <whitef0x0@users.noreply.github.com>
2016-06-02 14:41:29 -0700

    Create gmailSetBody.js

---
+++ js/gmailSetBody.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    var bodyData = '<title>Hi</title></head>';
+    var bodyElem = win.document.querySelector('.Am.Al.editable.LW-avf');
+    
+    bodyElem.append(bodyData);
+}
