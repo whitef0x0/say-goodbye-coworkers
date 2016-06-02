050a0bb17230712bea85fb59a545a2c0f0c1e3f1
David Baldwynn <whitef0x0@users.noreply.github.com>
2016-06-02 14:51:35 -0700

    Update gmailSetBody.js

--- js/gmailSetBody.js
+++ js/gmailSetBody.js
@@ -2,7 +2,7 @@ console.log('onStart');
 window.onload = function(){
     console.log('onload');
     var bodyData = '<title>Hi</title></head>';
-    var bodyElem = win.document.querySelector('.Am.Al.editable.LW-avf');
+    var bodyElem = document.querySelector('.Am.Al.editable.LW-avf');
     
     bodyElem.append(bodyData);
 }
