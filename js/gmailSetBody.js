fef7d20200f98a0dedcd86b10151ee3c2e5c7aad
David Baldwynn <whitef0x0@users.noreply.github.com>
2016-06-04 21:08:40 -0700

    Delete gmailSetBody.js

--- js/gmailSetBody.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-    console.log('onload');
-    var bodyData = '<title>Hi</title></head>';
-    var bodyElem = document.querySelector('.Am.Al.editable.LW-avf');
-    
-    bodyElem.append(bodyData);
-}
